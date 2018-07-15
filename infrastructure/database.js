cceee1ba2a3daab5f57450ca088da85e1018bbf9
abraham <alarrazolo@palo-it.com>
2018-07-15 18:08:09 -0500

    Remove db data connection.

--- infrastructure/database.js
+++ infrastructure/database.js
@@ -1,8 +1,8 @@
 var pg = require('pg');
 
-var conString = "postgres://postgres:password@localhost:5433/cuin?sslmode=disable";
+//var conString = "postgres://postgres:password@localhost:5433/cuin?sslmode=disable";
 
-module.exports= new pg.Client(conString);
+module.exports= new pg.Client();
 
 
 
