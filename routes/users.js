00cf8de6d1cf4e2d43d6d799516d638e7656f25f
abraham <alarrazolo@palo-it.com>
2018-07-15 20:32:12 -0500

    Add configuration of mail.

--- routes/users.js
+++ routes/users.js
@@ -6,67 +6,67 @@ var path = require('path');
 var fs = require("fs");
 var service = require("../domain/services/userService")
 var User = require("../domain/model/user")
+var transporter = require("../infrastructure/mail")
 
 
 const pug = require('pug');
 
 
-
 // Send email
-router.get('/',function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 
 });
 
-router.post('/',function (req,res,next) {
-    req.checkBody("email","Enter a valid email").isEmail();
-    req.checkBody("name","Enter a valid name").not().isEmpty();
-    req.checkBody("phone","Enter a valid phone").isMobilePhone('any');
-    req.checkBody("message","Enter a valid message").not().isEmpty();
+router.post('/', function (req, res, next) {
+    req.checkBody("email", "Enter a valid email").isEmail();
+    req.checkBody("name", "Enter a valid name").not().isEmpty();
+    req.checkBody("phone", "Enter a valid phone").isMobilePhone('any');
     var errors = req.validationErrors();
     if (errors) {
         res.send(errors);
         return;
     } else {
-       let user = new User(0,req.body.name,req.body.email,req.body.phone);
+        let user = new User(0, req.body.name, req.body.email, req.body.phone);
 
-       var data = {
-            sender:'contact@ebreika.com',
-            receiver:req.body.email,
-            name:req.body.name,
-            phone:req.body.phone,
-            message:req.body.message
+        var data = {
+            sender: process.env.MAIL_USER,
+            receiver: req.body.email,
+            name: req.body.name,
+            phone: req.body.phone,
         };
-     service.saveUser(user)
-        //sendMailToCustomer(data);
+        service.saveUser(user)
+        sendMailToCustomer(data);
         //sendMailToOwner(data);
-
-        res.json({ message: 'email send!' });
+        res.json({message: 'OK'});
     }
-
 
 
 })
 
 
 // Send email for customer
-function sendMailToCustomer(data){
-    let sendgridkey = process.env.SENDGRID_KEY;
-    sgMail.setApiKey(sendgridkey);
-    var file=fs.readFileSync(path.join(__dirname, '../public/template.html'), "utf8");
+function sendMailToCustomer(data) {
+    var file = fs.readFileSync(path.join(__dirname, '../public/template.html'), "utf8");
     const msg = {
-        to: data.receiver,
         from: data.sender,
-        subject: 'Cita Adagio',
+        to: data.receiver,
+        subject: 'Informes CUIN',
         text: 'Nos pondremos en contacto lo mas pronto posible',
         html: file
     }
-    sgMail.send(msg);
+    // send mail with defined transport object
+    transporter.sendMail(msg, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
 }
 
 
 // Send email for customer
-function sendMailToOwner(data){
+function sendMailToOwner(data) {
     let sendgridkey = process.env.SENDGRID_KEY;
     sgMail.setApiKey(sendgridkey);
     const compiledFunction = pug.compileFile(path.join(__dirname, '../templates/email-information.pug'));
@@ -75,10 +75,10 @@ function sendMailToOwner(data){
         from: data.sender,
         subject: 'Cita Adagio',
         text: 'Nos pondremos en contacto lo mas pronto posible',
-        html: compiledFunction({ name: data.name ,phone:data.phone,email: data.receiver,message:data.message  })
+        html: compiledFunction({name: data.name, phone: data.phone, email: data.receiver, message: data.message})
     }
     sgMail.send(msg);
 }
 
 
-module.exports=router
+module.exports = router
